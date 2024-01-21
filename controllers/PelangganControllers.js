a81a1d6875b3909c732010b70d5cde9fce716b5b
AgungCans11 <agungcans11@gmail.com>
2024-01-21 21:13:29 +0700

    df

--- controllers/PelangganControllers.js
+++ controllers/PelangganControllers.js
@@ -2,8 +2,6 @@ const { Sequelize } = require("sequelize");
 const PelangganModel = require("../models/PelangganModels");
 const RekapModel = require("../models/RekapModels");
 const db = require("../config/db");
-const wbm = require('wbm');
-
 exports.get = async (req, res) => {
   const page = parseInt(req.query.page) || 1;
   const pageSize = parseInt(req.query.pageSize) || 10;
@@ -130,7 +128,7 @@ exports.complete = async (req, res, next) => {
       no_telp: get.no_telp,
       harga: req.body.harga
     });
-    res.status(200).json({ success: true, msg: "Data berhasil disimpan direkap!" });
+    next();
   } catch (error) {
     res.status(500).json({ success: false, msg: error.message });
   }
